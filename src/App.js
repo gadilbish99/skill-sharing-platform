625709ef7307bba1700733242c545f1abc3e78a0
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-09 14:19:35 +0800

    Add App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import MainPage from './pages/MainPage';
 
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/" component={MainPage} />
+      </Switch>
+    </Router>
   );
 }
 
