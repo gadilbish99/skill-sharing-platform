22c19a68307e9319577fe9dbafdfbca1af7195ef
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-10 12:50:57 +0800

    Add Context

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ import {
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import MainPage from './pages/MainPage';
+import { createContext } from "react";
+import { useState } from "react";
 
+export const UserContext = createContext([]);
 
 function App() {
+  const [user, setUser] = useState({});
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/signup" component={SignUp} />
-        <Route path="/signin" component={SignIn} />
-        <Route path="/" component={MainPage} />
-      </Switch>
-    </Router>
+    <UserContext.Provider value={[user, setUser]}>
+      <Router>
+        <Switch>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/" component={MainPage} />
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
