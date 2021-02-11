93a2a5b9cd682d08d59adc07c9e8d24273ae8169
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-12 02:38:58 +0800

    Add axios interceptors

--- src/Services/service.js
+++ src/Services/service.js
@@ -1,5 +1,49 @@
 import axios from "axios";
-import { API_URL, BASE_URL, DEFAULT_CONFIG, UPLOAD_CONFIG, AUTH_CONFIG } from "./config";
+import { API_URL, BASE_URL, DEFAULT_CONFIG, UPLOAD_CONFIG } from "./config";
+import { getToken, setToken } from "../Utils/cookie";
+
+axios.interceptors.request.use(async (config) => {
+    if (getToken()) {
+        config.headers = {
+            'Authorization': `Bearer ${getToken()}`,
+            'Accept': 'application/json'
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(async (response) => {
+// Any status code that lie within the range of 2xx cause this function to trigger
+// Do something with response data
+    // console.log(`Yes it's 2xx`);
+    // console.log(response.data)
+    return response;
+}, async (error) => {
+// Any status codes that falls outside the range of 2xx cause this function to trigger
+// Do something with response error
+    // console.log(`No it's 4xx`);
+    // console.log(error.response.status);
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        try {
+            const { accesstoken } = await refreshToken();
+            setToken(accesstoken);
+            // console.log(error.response)
+            // console.log(typeof originalRequest.data);
+            if (originalRequest.data) {
+                originalRequest.data = JSON.parse(originalRequest.data)
+            }
+            return axios(originalRequest);
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+    // console.log(error.response);
+    return Promise.reject(error);
+});
 
 export async function getPosts() {
     try {
@@ -21,7 +65,15 @@ export async function upload(formData) {
 
 export async function postBlog(formData) {
     try {
-        const response = await axios.post(API_URL + '/post', formData, AUTH_CONFIG);
+        // const response = await axios.post(API_URL + '/post', formData, DEFAULT_CONFIG);
+        // return response.data;
+        const response = await axios({
+            url: API_URL + '/post',
+            method: 'post',
+            headers: DEFAULT_CONFIG.headers,
+            withCredentials: 'include',
+            data: formData
+        });
         return response.data;
     } catch (error) {
         throw error;
@@ -49,6 +101,20 @@ export async function signUp(formData) {
 export async function login(formData) {
     try {
         const response = await axios.post(API_URL + '/user/login', formData, DEFAULT_CONFIG);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function refreshToken() {
+    try {
+        const response = await axios({
+            url: API_URL + '/user/refresh_token',
+            method: 'post',
+            headers: DEFAULT_CONFIG.headers,
+            withCredentials: 'include'
+        });
         return response.data;
     } catch (error) {
         throw error;
