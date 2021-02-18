6286b0516891a468fa89ec934d1e2248ff887410
gadilbish99 <g.adilbish.99@gmail.com>
2021-02-18 20:35:04 +0800

    Change GridContainer Fetching

--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -1,12 +1,11 @@
 import Grid from '@material-ui/core/Grid';
 import Card from './Card';
 import { makeStyles } from '@material-ui/core/styles';
-import { useEffect, useState } from 'react';
-// import {
-//   useParams
-// } from "react-router-dom";
+import { useContext, useEffect, useState } from 'react';
 import Loading from '../components/Loading';
 import { getPosts } from '../Services/service';
+import { UserContext } from '../App';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
@@ -21,17 +20,34 @@ export default function GridContainer() {
   const classes = useStyles();
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [user] = useContext (UserContext);
+  const history = useHistory();
   // let { topic } = useParams();
   // topic = topic || '';
 
   useEffect(() => {
     async function fetchPosts() {
-      const posts = await getPosts();
-      setPosts(posts);
-      setLoading(false);
+      if (user.accesstoken) {
+        try {
+          const posts = await getPosts();
+          setPosts(posts);
+        } catch (error) {
+          const errorMsg = error.response.data.error;
+          setPosts([]);
+          if (errorMsg === 'jwt expired') {
+            history.push('/logout');
+          }
+        } finally {
+          setLoading(false);
+        }
+      }
+      else {
+        setPosts([]);
+        setLoading(false);
+      }
     }
     fetchPosts();
-  }, []);
+  }, [user, history]);
 
   if (loading) return <Loading />;
 
